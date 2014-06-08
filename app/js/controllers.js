fd8e72f65d5d61734bf2610e55b58051aaf67f64
Mark C Mitchell <mark@markcmitchell.net>
2014-06-08 22:41:42 +1000

    Table in place, ng-click hooked up, working on modal

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,20 +9,25 @@ angular.module('myApp.controllers', [])
     // hardDrives listing
     $scope.hardDrives = [
       {
-        name: "test",
-        size: "test",
-        notes: "test",
-        isOut: false
+        name: "Hard Drive 1",
+        size: "500GB",
+        notes: "This is a test note.",
+        event: {
+          location: "this is a location",
+          date: Date.now(),
+          notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu consectetur nisl. Fusce nec metus magna. Mauris enim arcu, venenatis quis ullamcorper eu, hendrerit semper erat. Aliquam faucibus luctus elit, congue pharetra justo faucibus feugiat. Morbi placerat quam non nisl luctus, ac dictum est laoreet. Aenean eget cursus nulla. Ut ante tortor, vehicula vel dictum ultrices, euismod a justo. Donec ac ultrices massa. Etiam sed lobortis est. Cras sit amet sagittis lacus. Quisque vitae sem lacus. Vestibulum dictum, felis et aliquam accumsan, ipsum tortor auctor augue, sit amet aliquet tortor elit posuere lacus. Aenean pellentesque, libero ut vulputate hendrerit, dolor massa sagittis nibh, non dignissim leo nisi id urna. Quisque lorem nunc, vehicula nec felis eu, sagittis pellentesque velit. Donec consequat eget augue sit amet luctus."
+        }
       }
     ];
-    // event listing
-    $scope.events = [
-      {
-        location: "",
-        date: "",
-        notes: ""
-      }
-    ];
+    
+    $scope.removeEvent = function(hardDrive) {
+      hardDrive.event = [];
+    };
+    
+    $scope.addEvent = function(hardDrive) {
+      // create a modal popup
+      $('#myModal').modal();
+    }
     
       
   }]);
