8f9e735daa4db98c96cfc884799770aa685ae73e
Mark C Mitchell <mark@markcmitchell.net>
2014-06-11 15:55:00 +1000

    add `version` and `author` services

--- app/js/services.js
+++ app/js/services.js
@@ -6,6 +6,8 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
+  .value('version', '0.1')
+  .value('author', 'Mark C Mitchell')
   .factory('eventService', function() {
     // containers for the real json objects to be passed through.
     var hardDrive = {};
