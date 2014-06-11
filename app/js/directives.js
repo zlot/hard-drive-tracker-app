8c32625d9ce905ec5e73d795f55a5cd629414dfd
Mark C Mitchell <mark@markcmitchell.net>
2014-06-11 14:25:36 +1000

    create add-form-modal-partial.html & addEventModal directive.

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,7 +5,19 @@
 
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+    // Directives that want to modify the DOM typically use the link option
+    // https://docs.angularjs.org/guide/directive
+    return {
+      restrict: 'A',
+      link: function(scope, elm, attrs) {
+        elm.text(version);
+      }   
     };
-  }]);
+  }])
+  .directive('addEventModal',function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/add-form-modal-partial.html'
+    };
+  })
+  ;
