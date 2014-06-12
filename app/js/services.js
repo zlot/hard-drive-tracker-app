2e8178204f53ed8c29316192cd07e97da87aac09
Mark C Mitchell <mark@markcmitchell.net>
2014-06-12 16:59:08 +1000

    version bump to v0.2

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-  .value('version', '0.1')
+  .value('version', '0.2')
   .value('author', 'Mark C Mitchell')
   .value('firebaseUniqueStr', 'REPLACE_ME_WITH_YOUR_FIREBASE_REFERENCE')
   .factory('HardDrivePassingService', function() {
